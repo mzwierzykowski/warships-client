8fd97b081f401ab4e9ad8528d871e07a38731fd4
Maciej Zwierzykowski <maciej.zwierzykowski@cshark.com>
2023-04-12 21:35:48 +0200

    Game start setup complete

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import { useState } from "react";
 import Board from "./components/Board";
 import GameStats from "./components/GameStats";
@@ -9,7 +10,7 @@ function App() {
     const [boardPoints, setBoardPoints] = useState([]);
     const [boardDimension, setBoardDimension] = useState({width: 0, height: 0});
     const [isFinished, setIsFinished] = useState(false);
-    const [stats, setGameStats] = useState({});
+    const [stats, setGameStats] = useState({shipStats: [], shotsFired:0, totalHits:0, totalMiss:0});
 
     const handleClick = async () => {
         console.log('clicked');
@@ -35,10 +36,11 @@ function App() {
 
     return (
         <div>
-            App component
-            <button onClick={handleClick}>Start</button>
-            <Board dimension={boardDimension} points={boardPoints} handlePointClick={handlePointClick}/>
-            <GameStats stats={stats}/>
+            <button className="new-game-button" onClick={handleClick}>New game</button>
+            <div className="game-components">
+                <Board dimension={boardDimension} points={boardPoints} handlePointClick={handlePointClick}/>
+                <GameStats stats={stats}/>
+            </div>
         </div>
     );
 }
