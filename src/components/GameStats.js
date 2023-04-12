e46260879227e35d8f3763ea277e89845c8d4018
Maciej Zwierzykowski <maciej.zwierzykowski@cshark.com>
2023-04-12 21:54:52 +0200

    User input handling

--- src/components/GameStats.js
+++ src/components/GameStats.js
@@ -2,7 +2,7 @@ import './GameStats.css'
 
 function GameStats({stats}) {
     const shipStats = stats.shipStats.map((shipStat) => {
-        return (<div className="ship-stat">
+        return (<div className="ship-stat" key={shipStat.type}>
             <p className="ship-type">{shipStat.type}: {shipStat.leftInGameCount}/{shipStat.totalCount}</p>
         </div>);
     });
@@ -14,7 +14,7 @@ function GameStats({stats}) {
             <div className="game-counters">
                 <p className="shots-fired-counter">Shoots fired: {stats.shotsFired}</p>
                 <p className="total-hits-counter">Total Hits: {stats.totalHits}</p>
-                <p className="total-miss-counter">Total Miss: {stats.totalHits}</p>
+                <p className="total-miss-counter">Total Miss: {stats.totalMiss}</p>
             </div>
         </div>
     );
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,8 +1,18 @@
 import './Tile.css';
 
 function Tile({point, handlePointClick}) {
+    const tileColors = [
+        'grey',
+        'blue',
+        'red'
+    ]
+    const handleClick = () => {
+        console.log('Tile clicked: ', point.id)
+        handlePointClick(point.id);
+    }
     return (
-        <div className="tile">
+        <div className="tile" onClick={handleClick}
+            style={{backgroundColor: tileColors[point.state]}}>
             {point.id}
         </div>
     );
